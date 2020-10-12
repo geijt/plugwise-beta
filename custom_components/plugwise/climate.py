b4424892be4d2978060de764bee7d831448c8239
Tom Scholten <git@scholten.nu>
2020-10-12 13:29:34 +0200

    Condense and also allow for COOL or other future states

--- custom_components/plugwise/climate.py
+++ custom_components/plugwise/climate.py
@@ -204,8 +204,8 @@ class PwThermostat(SmileGateway, ClimateEntity):
     async def async_set_hvac_mode(self, hvac_mode):
         """Set the hvac mode."""
         _LOGGER.debug("Set hvac_mode to: %s", hvac_mode)
+        state = SCHEDULE_ON
         if hvac_mode == HVAC_MODE_AUTO:
-            state = SCHEDULE_ON
             try:
                 await self._api.set_temperature(self._loc_id, self._schedule_temp)
                 self._setpoint = self._schedule_temp
@@ -213,30 +213,23 @@ class PwThermostat(SmileGateway, ClimateEntity):
                 self.async_write_ha_state()
             except Smile.PlugwiseError:
                 _LOGGER.error("Error while communicating to device")
-        elif hvac_mode == HVAC_MODE_HEAT:
-            state = SCHEDULE_OFF
-            try:
-                await self._api.set_schedule_state(
-                    self._loc_id, self._last_active_schema, state
-                )
-                self._hvac_mode = hvac_mode
-                self.async_write_ha_state()
-            except Smile.PlugwiseError:
-                _LOGGER.error("Error while communicating to device")
-        elif hvac_mode == HVAC_MODE_OFF:
-            state = SCHEDULE_OFF
-            preset_mode = "away"
-            try:
-                await self._api.set_schedule_state(
-                    self._loc_id, self._last_active_schema, state
-                )
-                self._hvac_mode = hvac_mode
+            return
+        state = SCHEDULE_OFF
+        try:
+            await self._api.set_schedule_state(
+                self._loc_id, self._last_active_schema, state
+            )
+            self._hvac_mode = hvac_mode
+            if hvac_mode == HVAC_MODE_OFF:
+                preset_mode = "away"
                 await self._api.set_preset(self._loc_id, preset_mode)
+
                 self._preset_mode = preset_mode
                 self._setpoint = self._presets.get(self._preset_mode, "none")[0]
-                self.async_write_ha_state()
-            except Smile.PlugwiseError:
-                _LOGGER.error("Error while communicating to device")
+            self.async_write_ha_state()
+        except Smile.PlugwiseError:
+            _LOGGER.error("Error while communicating to device")
+            return
 
     async def async_set_preset_mode(self, preset_mode):
         """Set the preset mode."""
